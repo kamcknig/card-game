f56b21970cd13bb1bed967ebf164c82e8cc4a7ed
Kyle McKnight <kamcknig@gmail.com>
2025-04-14 12:45:41 -0400

    added back original assets, and added assets for seaside expansion

--- server/src/utils/create-card.ts
+++ server/src/utils/create-card.ts
@@ -1,15 +1,18 @@
 import { Card, CardKey } from 'shared/shared-types.ts';
 import { ExpansionCardData } from '../state/expansion-data.ts';
+import { capitalize } from "es-toolkit/compat";
 
 let CARD_COUNT: number = 0;
 
 export const createCardFactory = (cardData: ExpansionCardData) => {
   return (cardKey: CardKey, card?: Omit<Partial<Card>, 'id'>): Card => {
+    const baseCardData = cardData.supply[cardKey] ?? cardData.kingdom[cardKey] ?? {};
     const c = new Card({
-      ...(cardData.supply[cardKey] ?? cardData.kingdom[cardKey]),
+      ...baseCardData,
       ...card ?? {},
       id: ++CARD_COUNT,
       cardKey: cardKey,
+      cardName: baseCardData.cardName ?? capitalize(cardKey),
     });
     console.log(`[createCard] created ${c}`);
     return c;
