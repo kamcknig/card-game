2ccd7dc1d7390ccd1523c13c500f801524807604
Kyle McKnight <kyle.mcknight.imisshtml@gmail.com>
2025-04-14 20:03:02 -0400

    started blockade

--- server/src/expansions/seaside/card-effects-seaside.ts
+++ server/src/expansions/seaside/card-effects-seaside.ts
@@ -1,6 +1,10 @@
 import { CardExpansionModule } from '../card-expansion-module.ts';
 import { GainTreasureEffect } from '../../core/effects/effect-types/gain-treasure.ts';
 import { GainBuyEffect } from '../../core/effects/effect-types/gain-buy.ts';
+import { DrawCardEffect } from '../../core/effects/effect-types/draw-card.ts';
+import { GainActionEffect } from '../../core/effects/effect-types/gain-action.ts';
+import { SelectCardEffect } from '../../core/effects/effect-types/select-card.ts';
+import { GainCardEffect } from '../../core/effects/effect-types/gain-card.ts';
 
 const expansion: CardExpansionModule = {
   registerCardLifeCycles: () => ({
@@ -38,6 +42,46 @@ const expansion: CardExpansionModule = {
       
       console.log(`[SEASON EFFECT] gaining 1 buy...`);
       yield new GainBuyEffect({ count: 1, sourcePlayerId: triggerPlayerId });
+    },
+    'bazaar': () => function* ({triggerPlayerId, triggerCardId}) {
+      console.log(`[SEASON EFFECT] drawing 1 card...`);
+      yield new DrawCardEffect({
+        playerId: triggerPlayerId,
+        sourcePlayerId: triggerPlayerId,
+        sourceCardId: triggerCardId
+      });
+      
+      console.log(`[SEASON EFFECT] gaining 2 actions...`);
+      yield new GainActionEffect({ count: 2, sourcePlayerId: triggerPlayerId });
+      
+      console.log(`[SEASON EFFECT] gaining 1 treasure...`);
+      yield new GainTreasureEffect({ count: 1, sourcePlayerId: triggerPlayerId });
+    },
+    'blockade': () => function* ({cardLibrary, triggerPlayerId, triggerCardId}) {
+      const cardIds = (yield new SelectCardEffect({
+        prompt: 'Gain card',
+        validPrompt: '',
+        playerId: triggerPlayerId,
+        sourcePlayerId: triggerPlayerId,
+        restrict: {
+          from: { location: ['supply', 'kingdom'] },
+          cost: { kind: 'upTo', amount: 4 },
+        },
+        count: 1,
+        sourceCardId: triggerCardId
+      })) as number[];
+      
+      const cardId = cardIds[0];
+      
+      console.log(`[BLOCKADE EFFECT] selected card ${cardLibrary.getCard(cardId)}`);
+      
+      yield new GainCardEffect({
+        playerId: triggerPlayerId,
+        sourcePlayerId: triggerPlayerId,
+        sourceCardId: triggerCardId!,
+        cardId,
+        to: { location: 'set-aside' },
+      });
     }
   }),
 }
