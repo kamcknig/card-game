c7f19034e5afaf5ddd43525342aa78fa64609f33
Kyle McKnight <kamcknig@gmail.com>
2025-05-03 14:30:05 -0400

    aadded basic effects for the young witch

---
+++ server/src/expansions/cornucopia/card-effects-cornucopia.ts
@@ -0,0 +1,41 @@
+import { CardId } from "shared/shared-types.ts";
+import { CardExpansionModule } from '../../types.ts';
+import { findOrderedTargets } from '../../utils/find-ordered-targets.ts';
+
+const expansion: CardExpansionModule = {
+  'young-witch': {
+    registerEffects: ()  => async (cardEffectArgs) => {
+      console.log(`[young witch effect] drawing 2 cards`);
+      await cardEffectArgs.runGameActionDelegate('drawCard', { playerId: cardEffectArgs.playerId, count: 2 });
+      
+      const count = Math.min(2, cardEffectArgs.match.playerHands[cardEffectArgs.playerId].length);
+      
+      console.log(`[young witch effect] selecting ${count} cards`);
+      
+      const selectedCardIds = await cardEffectArgs.runGameActionDelegate('selectCard', {
+        playerId: cardEffectArgs.playerId,
+        prompt: `Discard cards`,
+        restrict: { from: { location: 'playerHands' } },
+        count,
+      }) as CardId[];
+      
+      if (!selectedCardIds.length) {
+        console.log(`[young witch effect] no cards selected`);
+      }
+      
+      const targetPlayerIds = findOrderedTargets({
+        match: cardEffectArgs.match,
+        appliesTo: 'ALL_OTHER',
+        startingPlayerId: cardEffectArgs.playerId
+      }).filter(playerId => cardEffectArgs.reactionContext?.[playerId]?.result !== 'immunity');
+      
+      for (const targetPlayerId of targetPlayerIds) {
+        const handIds = cardEffectArgs.match.playerHands[targetPlayerId];
+        const handCards = handIds.map(cardId => cardEffectArgs.cardLibrary.getCard(cardId));
+        const baneCards = handCards.filter(card => card.type.includes('bane'));
+      }
+    }
+  }
+}
+
+export default expansion;
