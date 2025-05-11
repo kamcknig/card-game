af2de29d3eaebaaadfff934e27ef4fe704741c54
Kyle McKnight <kamcknig@gmail.com>
2025-05-11 08:57:22 -0400

    altar aadded

---
+++ server/src/expansions/dark-ages/card-effects-dark-ages.ts
@@ -0,0 +1,54 @@
+import { CardId } from 'shared/shared-types.ts';
+import { CardExpansionModule } from '../../types.ts';
+
+const cardEffects: CardExpansionModule = {
+  'altar': {
+    registerEffects: () => async (cardEffectArgs) => {
+      let selectedCardIds = await cardEffectArgs.runGameActionDelegate('selectCard', {
+        playerId: cardEffectArgs.playerId,
+        prompt: `Trash card`,
+        restrict: { location: 'playerHand', playerId: cardEffectArgs.playerId },
+        count: 1,
+      }) as CardId[];
+      
+      if (!selectedCardIds.length) {
+        console.warn(`[dark-ages] no card selected`);
+        return;
+      }
+      
+      const cardToTrash = cardEffectArgs.cardLibrary.getCard(selectedCardIds[0]);
+      
+      await cardEffectArgs.runGameActionDelegate('trashCard', {
+        playerId: cardEffectArgs.playerId,
+        cardId: cardToTrash.id,
+      });
+      
+      selectedCardIds = await cardEffectArgs.runGameActionDelegate('selectCard', {
+        playerId: cardEffectArgs.playerId,
+        prompt: `Gain card`,
+        restrict: [
+          { location: ['basicSupply', 'kingdomSupply'] },
+          { kind: 'upTo', amount: { treasure: 5 }, playerId: cardEffectArgs.playerId }
+        ],
+        count: 1,
+      }) as CardId[];
+      
+      if (!selectedCardIds) {
+        console.log(`[dark-ages] no card selected`);
+        return;
+      }
+      
+      const cardToGain = cardEffectArgs.cardLibrary.getCard(selectedCardIds[0]);
+      
+      console.log(`[dark-ages] gaining card ${cardToGain}`);
+      
+      await cardEffectArgs.runGameActionDelegate('gainCard', {
+        playerId: cardEffectArgs.playerId,
+        cardId: cardToGain.id,
+        to: { location: 'playerDiscard' }
+      });
+    }
+  },
+}
+
+export default cardEffects;
