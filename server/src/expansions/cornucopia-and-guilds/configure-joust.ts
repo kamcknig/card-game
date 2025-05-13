5af2f1b9bd89e0d6b438e35917e2b4fa4b1e72bc
Kyle McKnight <kamcknig@gmail.com>
2025-05-13 14:45:30 -0400

    cleaned up the configuration of joust

--- server/src/expansions/cornucopia-and-guilds/configure-joust.ts
+++ server/src/expansions/cornucopia-and-guilds/configure-joust.ts
@@ -1,6 +1,6 @@
-import { CardNoId, Supply } from 'shared/shared-types.ts';
+import { Supply } from 'shared/shared-types.ts';
 import { ExpansionConfiguratorContext } from '../../types.ts';
-import { createCardData } from '../../utils/create-card-data.ts';
+import { expansionLibrary } from '../expansion-library.ts';
 
 export const configureJoust = async (args: ExpansionConfiguratorContext) => {
   const joustPresent = args.config.kingdomSupply.some(supply => supply.name === 'joust');
@@ -11,18 +11,6 @@ export const configureJoust = async (args: ExpansionConfiguratorContext) => {
   
   console.log(`[cornucopia configurator - configuring joust] joust present in supply`);
   
-  let rewardCardLibrary: Record<string, CardNoId> = {};
-  
-  try {
-    const rewardCardLibraryModule = await import('./card-library-rewards.json', { with: { type: 'json' } });
-    rewardCardLibrary = rewardCardLibraryModule.default as unknown as Record<string, CardNoId>;
-  } catch (error) {
-    if ((error as any).code !== 'ERR_MODULE_NOT_FOUND') {
-      console.warn(`[cornucopia configurator - configuring joust] failed to load reward library for joust`);
-      return;
-    }
-  }
-  
   args.config.nonSupply ??= [];
   
   const rewardsKingdom = {
@@ -32,10 +20,15 @@ export const configureJoust = async (args: ExpansionConfiguratorContext) => {
   
   args.config.nonSupply.push(rewardsKingdom);
   
-  for (const key of Object.keys(rewardCardLibrary ?? {})) {
-    const cardData = createCardData(key, 'cornucopia-and-guilds', {
-      ...rewardCardLibrary[key] ?? {},
-    });
+  const expansionData = expansionLibrary['cornucopia-and-guilds'];
+  const expansionKingdomCards = expansionData.cardData.kingdomSupply;
+  const rewards = Object.keys(expansionKingdomCards).filter(key => expansionKingdomCards[key].type.includes('REWARD'));
+  
+  for (const key of  rewards ?? []) {
+    const cardData = {
+      ...structuredClone(expansionKingdomCards[key]) ?? {},
+      partOfSupply: false,
+    };
     const count = args.config.players.length > 2 ? 2 : 1;
     rewardsKingdom.cards = [...rewardsKingdom.cards, ...new Array(count).fill(cardData)];
   }
